c2229f30f778e1f896723f9fd2760268b0cbddae
Douglas Graham <dig2@rice.edu>
2023-08-08 11:12:00 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+        extended: true
+}));
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+
+        res.sendFile(__dirname + "/public/skyline.html");
+
+});
+
+
+app.listen(3000, function () {
+        console.log("Server started on port 3000");
+});
