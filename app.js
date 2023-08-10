9ba8c5cd9b075fb35e5570d70ff3b870d8e4bf68
Douglas Graham <dig2@rice.edu>
2023-08-10 12:15:31 -0700

    finished practice registration form

--- app.js
+++ app.js
@@ -3,21 +3,19 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-        extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
-
 app.get("/", (req, res) => {
-
-        res.sendFile(__dirname + "/public/skyline.html");
-
+  res.sendFile(__dirname + "/public/reg-form.html");
 });
 
-
 app.listen(3000, function () {
-        console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
